173e13e92f49e6e14a4858b2b256bc57a7f12fce
Jyothika-Bollareddy <bollareddyjyothika21@gmail.com>
2025-03-23 19:33:39 -0500

    Add files via upload

---
+++ works/7/source.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = x + 5;
+
+
+y +=10
+console.log(y);
+y -= 10
+console.log(y);
+y *= 10
+console.log(y);
+y /=10
+console.log(y);
+y %= 10
+console.log(y);
+y **=10
+console.log(y);
+y++;
+y--;
+console.log(y);
